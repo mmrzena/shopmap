67877879d0c39b3f7a217e841618bc6ddf2411e9
Michal Mrzena <michal.mrzena@socialbakers.com>
2021-04-08 20:36:20 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet'
+import {shops} from './shops'
+
+import aramaraMarkerIcon from './aramara_marker.png'
+
+const DefaultIcon = L.icon({
+    iconUrl: aramaraMarkerIcon,
+		iconSize: [30,60],
+		iconAnchor: [15,60],
+		popupAnchor: [0,-60]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const hasDiv = window.document.getElementById('shopmap')
+
+if (hasDiv) {
+	const mymap = L.map('shopmap').setView([50.4353, 15.3610], 9);
+	
+	const createMarker = ({name, address, lat, lng}) => {
+		const marker = L.marker([lat, lng]).addTo(mymap);
+		const popup = L.popup()
+		.setLatLng([lat, lng])
+		.setContent(`<p class="popup_name">${name}</p><p>${address}</p>`)
+		marker.bindPopup(popup);
+	}
+	// load a tile layer
+	L.tileLayer('https://mapserver.mapy.cz/turist-m/{z}-{x}-{y}',
+	{
+		attribution: '<a href="https://o.seznam.cz/">© Seznam.cz, a.s.</a> a OpenStreetMap',
+		maxZoom: 18,
+		minZoom: 8
+	}).addTo(mymap);
+	
+	shops.forEach((shop) => createMarker(shop))
+}
+</script>
