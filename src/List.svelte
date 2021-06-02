b9578d5ec141537c56e7412506abc5a4afc87ed8
Michal Mrzena <michal.mrzena@socialbakers.com>
2021-06-02 15:49:31 +0200

    add more stuff

--- src/List.svelte
+++ src/List.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import shops from '../server/shops.json';
 
-	const shopsToList = shops.filter((item) => !item.name.startsWith('Zde pro vás vyrábíme'))
+	const shopsToList = shops.filter((item) => !item.name.startsWith('Zde pro vás vyrábíme')).sort((item1, item2) => {
+		return item1.name.localeCompare(item2.name);
+	})
 </script>
 
 <style>
